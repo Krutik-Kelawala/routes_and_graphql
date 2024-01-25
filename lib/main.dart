c8d97dfe8c0a45c4e436c0eb9f3a568ed04dbd07
Krutik-Kelawala <104120613+Krutik-Kelawala@users.noreply.github.com>
2024-01-25 21:52:15 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,8 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        showPerformanceOverlay: true,
+      // TODO When app page load performance show in real device uncommet below line
+       // showPerformanceOverlay: true,
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         onGenerateRoute: router.routeController,
